161d1196ff62bebad007ae2c497b0d098c9799ca
Piscescup <piscescup@qq.com>
2026-01-13 15:45:14 +0800

    refactor(itemgroup_register); add Javadoc for the ItemGroupRegister.

--- src/main/java/io/github/piscescup/mc/fabric/register/itemgroup/ItemGroupPreRegistrable.java
+++ src/main/java/io/github/piscescup/mc/fabric/register/itemgroup/ItemGroupPreRegistrable.java
@@ -13,6 +13,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+
+
 /**
  * A pre-registration builder API for creating and registering an
  * {@link ItemGroup}.
